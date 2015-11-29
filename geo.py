612d90c5664045debeefdba074a1c33914a5c4f5
a2ohm <antoine@2ohm.fr>
2015-11-29 21:16:00 +0100

    In the geo class, separate variable to store the name's root of the document, the input file name and the output file name.

--- geo.py
+++ geo.py
@@ -13,31 +13,37 @@ print("---------------------------- geo --")
 print("-- by antoine.delhomme@espci.org --")
 print("-----------------------------------")
 
-doc_in = "./001-v2-doc.md"
+doc_in = "./001-v2-doc"
 
 class geoReader():
 
-    def __init__(self, doc_in):
-        self.doc_in = doc_in
+    def __init__(self, doc_name):
+        self.doc_name = doc_name
+        self.doc_in = "%s.md" % self.doc_name
+        self.doc_out = "%s.html" % self.doc_name
+
         self.header = None
         self.header_limit = -1
 
     def __enter__(self):
         """Open the file.
         """
-        self.f = open(self.doc_in, 'r')
+
+        self.f_in = open(self.doc_in, 'r')
+        self.f_out = open(self.doc_out, 'w')
 
         return self
 
     def __exit__(self, type, value, traceback):
         """Close the file.
         """
-        self.f.close()
+        self.f_in.close()
+        self.f_out.close()
 
     def parseHeader(self):
         """Parse the header of the file.
         """
-        s = mmap.mmap(self.f.fileno(), 0, access=mmap.ACCESS_READ)
+        s = mmap.mmap(self.f_in.fileno(), 0, access=mmap.ACCESS_READ)
         self.header_limit = s.find(b'---')
 
         if self.header_limit != -1:
