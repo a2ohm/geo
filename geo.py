912a2c23ac73856e55c6c6e386c50f8a31804b0b
a2ohm <antoine@2ohm.fr>
2015-11-29 21:06:11 +0100

    Beginning of the project.

---
+++ geo.py
@@ -0,0 +1,52 @@
+#! /usr/bin/python3
+# -*- coding-utf-8 -*-
+
+"""
+This script transform a md into a plain html in the context of a
+documentation for Kit&Pack.
+"""
+
+import mmap
+import yaml
+
+print("---------------------------- geo --")
+print("-- by antoine.delhomme@espci.org --")
+print("-----------------------------------")
+
+doc_in = "./001-v2-doc.md"
+
+class geoReader():
+
+    def __init__(self, doc_in):
+        self.doc_in = doc_in
+        self.header = None
+
+    def __enter__(self):
+        """Open the file.
+        """
+        self.f = open(self.doc_in, 'r')
+
+        return self
+
+    def __exit__(self, type, value, traceback):
+        """Close the file.
+        """
+        self.f.close()
+
+    def parseHeader(self):
+        """Parse the header of the file.
+        """
+        s = mmap.mmap(self.f.fileno(), 0, access=mmap.ACCESS_READ)
+        m = s.find(b'---')
+
+        if m != -1:
+            self.header = yaml.load(s[0:m])
+            print(self.header['name'])
+        else:
+            print("Cannot load the header")
+
+
+
+# Read the document
+with geoReader(doc_in) as g:
+    g.parseHeader()
